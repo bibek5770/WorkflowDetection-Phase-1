49348990bef4d29a35e0d6e39dfde0c23a7c38c0
unknown <seanlukecraft@hotmail.com>
2015-11-10 14:05:58 -0600

    before changes

---
+++ Assets/Scripts/Properties.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Properties : MonoBehaviour {
+
+    public string[] properties;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
